1c25ff5f16854d89e7ee8504342d1912ec483bf4
Jeff Knaggs <mail@jeef3.com>
2018-05-11 14:18:30 +1200

    Hook up links

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import cheerio from 'cheerio';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -11,21 +12,59 @@ export default class App extends React.Component {
     fetch('http://localhost:5000/sites.json')
       .then(res => res.json())
       .then(sites => {
-        const s = sites.filter(si => si.host.port !== 3000);
-        console.log(s);
+        // TODO: temp, ignore this static serve
+        const s = sites.filter(si => si.host.port !== 3000)
+          .map(si => {
+            const $ = cheerio.load(si.html);
+            const title = $('title').text() || 'Unknown';
+            return { ...si, title, url: `http://localhost:${si.host.port}` };
+          });
+
         this.setState({ sites: s });
       });
   }
 
   render() {
 
-    return (
-    <div>
-      Sites
+    const thumbnailWidth = 200;
+    const thumbnailHeight = 150;
+    const scale = 0.25;
 
+    return (
+      <div style={{
+        width: '100vw',
+        height: '100vh'
+      }}>
       {this.state.sites.map(site => (
         <div key={site.host.pid}>
-          <iframe src={`http://localhost:${site.host.port}`}></iframe>
+          <h2>{site.title}</h2>
+          <a
+            href={site.url}
+            style={{
+              display: 'block',
+            width: thumbnailWidth,
+            height: thumbnailHeight,
+            border: 'solid 1px gray',
+
+            cursor: 'pointer',
+          }}
+        >
+          <div style={{
+            overflow: 'hidden',
+            width: thumbnailWidth,
+            height: thumbnailHeight,
+            pointerEvents: 'none'
+          }}>
+            <iframe
+              src={site.url}
+              style={{
+                width: (thumbnailWidth / scale), height: (thumbnailHeight / scale), border: 0,
+                transform: `scale(${scale})`,
+                transformOrigin: 'top left'
+              }}
+            ></iframe>
+          </div>
+        </a>
         </div>
       ))}
     </div>);
